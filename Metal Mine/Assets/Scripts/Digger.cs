8a15ee1984a38fec92f2ee480dd69f6114c59da5
grimscyth <grimscyth@gmail.com>
2015-10-20 18:43:57 -0700

    New Classes Added
    Added in the class for digger.

---
+++ Metal Mine/Assets/Scripts/Digger.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Digger : Worker {
+
+    const int MAX_DIG = 10;
+    const int MAX_KNOWLEDGE = 10;
+    const int MAX_TREASURE = 10;
+    const int MAX_LEVEL = 10;
+
+    // TRAITS
+    int digSpeed;
+    int pickKnowledge;
+    int treasure;
+
+    int pickPower;
+    int digPower;
+    int totalDigPower;
+
+    bool mineCartNearby;
+    bool isDigging;
+
+	void Start () 
+    {
+        name = "Digger";
+
+        digSpeed = 1;
+        pickKnowledge = 1;
+        treasure = 1;
+
+        pickPower = 1;
+        digPower = 1;
+        
+        setTotalDigPower();
+	}
+
+    public int getDigSpeed()
+    {
+        return digSpeed;
+    }
+
+    public bool incDigSpeed()
+    {
+        if (digSpeed < MAX_DIG)
+        {
+            digSpeed++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getPickKnowledge()
+    {
+        return pickKnowledge;
+    }
+
+    public bool incPickKnowledge()
+    {
+        if (pickKnowledge < MAX_KNOWLEDGE)
+        {
+            pickKnowledge++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getTreasure()
+    {
+        return treasure;
+    }
+
+    public bool incTreasure()
+    {
+        if (treasure < MAX_TREASURE)
+        {
+            treasure++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void setTotalDigPower()
+    {
+        digPower = (digSpeed * level) + (pickKnowledge * pickPower);
+    }
+}
--- Metal Mine/Assets/Scripts/Worker.cs
+++ Metal Mine/Assets/Scripts/Worker.cs
@@ -3,177 +3,73 @@ using System.Collections;
 
 public class Worker : MonoBehaviour 
 {
-    string name;
-    int diggerLevel;
-    int diggerExperience;
-    int haulerLevel;
-    int haulerExperience;
-    int sifterLevel;
-    int sifterExperience;
-    int gemCollectorExperience;
-    int gemCollectorLevel;
-    int structuralEngineerLevel;
-    int structuralEngineerExperience;
+    GameController gameController;
 
-    public Worker(string nName = "Bob")
+    //UserInterface userInterface;
+
+    protected string workerName;
+    protected int experience;
+    protected int level;
+
+    public void Start()
     {
-        name = nName;
-        diggerLevel = 1;
-        diggerExperience = 0;
-        haulerLevel = 1;
-        haulerExperience = 0;
-        sifterLevel = 1;
-        sifterExperience = 0;
-        gemCollectorLevel = 1;
-        gemCollectorExperience = 0;
-        structuralEngineerLevel = 1;
-        structuralEngineerExperience = 0;
+        workerName = "Bob";
+        experience = 0;
+        level = 1;
+
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        //userInterface = gameController.GetComponent<UserInterface>();
     }
 
     public void changeName(string nName)
     {
-        name = nName;
+        workerName = nName;
     }
 
-    public int getDiggerLevel()
+    public string getName()
     {
-        return diggerLevel;
+        return workerName;
     }
 
-    public void incrementDiggerLevel()
+    private bool checkLevelUp(int level)
     {
-        diggerLevel++;
-    }
-
-    public int getDiggerExperience()
-    {
-        return diggerExperience;
-    }
-
-    public void increaseDiggerExperience(int exp)
-    {
-        diggerExperience += exp;
-    }
-
-    public int getHaulerLevel()
-    {
-        return haulerLevel;
-    }
-
-    public void incrementHaulerLevel()
-    {
-        haulerLevel++;
-    }
-
-    public int getHaulerExperience()
-    {
-        return haulerExperience;
-    }
-
-    public void increaseHaulerExperience(int exp)
-    {
-        haulerExperience += exp;
-    }
-
-    public int getSifterLevel()
-    {
-        return sifterLevel;
-    }
-
-    public void incrementSifterLevel()
-    {
-        sifterLevel++;
-    }
-
-    public int getSifterExperience()
-    {
-        return sifterExperience;
-    }
-
-    public void increaseSifterExperience(int exp)
-    {
-        sifterExperience += exp;
-    }
-
-    public int getGemCollectorLevel()
-    {
-        return gemCollectorLevel;
-    }
-
-    public void incrementGemCollectorLevel()
-    {
-        gemCollectorLevel++;
-    }
-
-    public int getGemCollectorExperience()
-    {
-        return gemCollectorExperience;
-    }
-
-    public void increaseGemCollectorExperience(int exp)
-    {
-        gemCollectorExperience += exp;
-    }
-
-    public int getStructuralEngineerLevel()
-    {
-        return structuralEngineerLevel;
-    }
-
-    public void incrementStructuralEngineerLevel()
-    {
-        structuralEngineerLevel++;
-    }
-
-    public int getStructuralEngineerExperience()
-    {
-        return structuralEngineerExperience;
-    }
-
-    public void increaseStructuralEngineerExperience(int exp)
-    {
-        structuralEngineerExperience += exp;
-    }
-
-    private bool checkLevelUp(int skillLevel, int exp)
-    {
-        if (skillLevel == 1 && exp >= 100)
+        if (level == 1 && experience >= 100)
         {
             return true;
         }
-        if (skillLevel == 2 && exp >= 200)
+        if (level == 2 && experience >= 200)
         {
             return true;
         }
-        if (skillLevel == 3 && exp >= 300)
+        if (level == 3 && experience >= 300)
         {
             return true;
         }
-        if (skillLevel == 4 && exp >= 400)
+        if (level == 4 && experience >= 400)
         {
             return true;
         }
-        if (skillLevel == 5 && exp >= 500)
+        if (level == 5 && experience >= 500)
         {
             return true;
         }
-        if (skillLevel == 6 && exp >= 600)
+        if (level == 6 && experience >= 600)
         {
             return true;
         }
-        if (skillLevel == 7 && exp >= 700)
+        if (level == 7 && experience >= 700)
         {
             return true;
         }
-        if (skillLevel == 8 && exp >= 800)
+        if (level == 8 && experience >= 800)
         {
             return true;
         }
-        if (skillLevel == 9 && exp >= 900)
+        if (level == 9 && experience >= 900)
         {
             return true;
         }
-        if (skillLevel == 10 && exp >= 1000)
+        if (level == 10 && experience >= 1000)
         {
             return true;
         }
